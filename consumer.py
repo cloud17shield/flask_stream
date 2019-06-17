a03ec90ad8f800c6fd6694b0845ae036281fb805
stormrise <lilingxiao0414@gmail.com>
2019-06-17 18:13:48 +0800

    socket_debug-6

--- consumer.py
+++ consumer.py
@@ -67,4 +67,4 @@ def socket_streaming():
 
 
 if __name__ == '__main__':
-    app.run(host="10.244.1.12", debug=True, port=54321)
+    app.run(host="10.244.1.12", debug=True, port=54321, threaded=True)
